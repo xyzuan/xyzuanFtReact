9ce2b3c17659dfa501801400cea7d42fafd113dd
xyzuan <xyzuannihboss@gmail.com>
2023-08-09 17:09:53 +0700

    main: Create preloader

---
+++ src/app/components/Preloader.tsx
@@ -0,0 +1,44 @@
+import { Container, Grid, LinearProgress } from "@mui/material";
+import Image from "next/image";
+import React, { useState, useEffect } from "react";
+import BorderLinearProgress from "./BorderLinearProgress";
+
+function Preloader() {
+  const [progress, setProgress] = useState(10);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((prevProgress) =>
+        prevProgress >= 100 ? 10 : prevProgress + 10
+      );
+    }, 100);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <Grid>
+        <Image height="200" width="200" alt="200" src="/assets/preloader.svg" />
+        <BorderLinearProgress
+          variant="determinate"
+          value={progress}
+          sx={{
+            marginTop: "58px",
+          }}
+        />
+      </Grid>
+    </Container>
+  );
+}
+
+export default Preloader;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import { Container } from "@mui/material";
+"use client";
+
 import Header from "./components/Header";
 import HeroSection from "./sections/Hero/HeroSection";
 import AboutSection from "./sections/About/AboutSection";
@@ -6,17 +7,39 @@ import Footer from "./components/Footer";
 import PortofolioSection from "./sections/Portfolio/PortofolioSection";
 import TimelineSection from "./sections/Timeline/TimelineSection";
 import ContactSection from "./sections/Contact/ContactSection";
+import { useEffect, useState } from "react";
+import Preloader from "./components/Preloader";
 
 export default function Home() {
+  const [showPreloader, setShowPreloader] = useState(true);
+  const [contentVisible, setContentVisible] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowPreloader(false);
+      setTimeout(() => {
+        setContentVisible(true);
+      }, 100);
+    }, 2000);
+  }, []);
+
   return (
-    <>
-      <Header />
-      <HeroSection />
-      <AboutSection />
-      <PortofolioSection />
-      {/* <TimelineSection /> */}
-      <ContactSection />
-      <Footer />
-    </>
+    <div className={`fade-animation ${contentVisible ? "hide-content" : ""}`}>
+      {showPreloader && <Preloader />}
+      <div
+        style={{
+          opacity: contentVisible ? 1 : 0,
+          transition: "opacity 0.3s ease-in-out",
+        }}
+      >
+        <Header />
+        <HeroSection />
+        <AboutSection />
+        <PortofolioSection />
+        {/* <TimelineSection /> */}
+        <ContactSection />
+        <Footer />
+      </div>
+    </div>
   );
 }

f477d2baa912af78020f4173e633028e6476cb80
xyzuan <xyzuannihboss@gmail.com>
2023-08-10 10:53:52 +0700

    main: meta: Initial SEO implementations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,7 @@
 import "./globals.css";
 import "./fonts.css";
 
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "xyzuan",
-  description: "",
-};
+import Head from "next/head";
 
 export default function RootLayout({
   children,
@@ -15,6 +10,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <title>xyzuan</title>
+        <meta
+          name="description"
+          content="Welcome to Jody Yuantoro's Portfolio Website. Explore a collection of my creative projects, designs, and accomplishments. Discover how I bring ideas to life through innovative design and development."
+        />
+        <meta property="og:title" content="xyzuan" />
+        <meta
+          property="og:description"
+          content="Welcome to Jody Yuantoro's Portfolio Website. Explore a collection of my creative projects, designs, and accomplishments. Discover how I bring ideas to life through innovative design and development."
+        />
+        <meta property="og:image" content="/assets/xyzuanbanner.png" />
+      </Head>
       <body>{children}</body>
     </html>
   );

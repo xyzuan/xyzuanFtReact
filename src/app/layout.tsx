fdef9831257672ee447fdc2cd015048267dfba0c
xyzuan <xyzuannihboss@gmail.com>
2023-08-14 21:44:31 +0700

    main: META Improvement

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import "./globals.css";
 import "./fonts.css";
 
-import Head from "next/head";
-
 export default function RootLayout({
   children,
 }: {
@@ -10,19 +8,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <title>xyzuan</title>
-        <meta
-          name="description"
-          content="Welcome to Jody Yuantoro's Portfolio Website. Explore a collection of my creative projects, designs, and accomplishments. Discover how I bring ideas to life through innovative design and development."
-        />
-        <meta property="og:title" content="xyzuan" />
-        <meta
-          property="og:description"
-          content="Welcome to Jody Yuantoro's Portfolio Website. Explore a collection of my creative projects, designs, and accomplishments. Discover how I bring ideas to life through innovative design and development."
-        />
-        <meta property="og:image" content="/assets/xyzuanbanner.png" />
-      </Head>
       <body>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,19 @@ import HeroSection from "./sections/Hero/HeroSection";
 import AboutSection from "./sections/About/AboutSection";
 import Footer from "./components/Footer";
 import PortofolioSection from "./sections/Portfolio/PortofolioSection";
-import TimelineSection from "./sections/Timeline/TimelineSection";
 import ContactSection from "./sections/Contact/ContactSection";
 import { useEffect, useState } from "react";
 import Preloader from "./components/Preloader";
+import Head from "next/head";
 
 export default function Home() {
+  const siteTitle =
+    "Jody Yuantoro | Frontend Developer &amp; Mobile UI/UX Designer";
+  const siteDesc =
+    "Welcome to Jody Yuantoro's Portfolio Website. Explore a collection of my creative projects, designs, and accomplishments. Discover how I bring ideas to life through innovative design and development.";
+  const siteKey =
+    "Jody Yuantoro, Frontend Developer, Mobile UI/UX Designer, Android Developer, Malang, Responsive Web Design, User Experience, Web Development, optimized user experiences, Malang-based";
+
   const [showPreloader, setShowPreloader] = useState(true);
   const [contentVisible, setContentVisible] = useState(false);
 
@@ -32,22 +39,42 @@ export default function Home() {
   }, [showPreloader]);
 
   return (
-    <div className={`fade-animation ${contentVisible ? "hide-content" : ""}`}>
-      {showPreloader && <Preloader />}
-      <div
-        style={{
-          opacity: contentVisible ? 1 : 0,
-          transition: "opacity 0.3s ease-in-out",
-        }}
-      >
-        <Header />
-        <HeroSection />
-        <AboutSection />
-        <PortofolioSection />
-        {/* <TimelineSection /> */}
-        <ContactSection />
-        <Footer />
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>{siteTitle}</title>
+        <meta property="title" content={siteTitle} />
+        <meta property="og:title" content={siteTitle} />
+        <meta name="description" content={siteDesc} />
+        <meta property="og:description" content={siteDesc} />
+        <meta name="keywords" content={siteKey} />
+        <meta
+          property="og:image"
+          content="https://raw.githubusercontent.com/xyzuan/xyzuanFtReact/main/public/assets/xyzuanbanner.png"
+        />
+        <meta property="og:image:alt" content="xyzuan" />
+        <meta property="og:url" content="https://xyzuan.my.id/" />
+        <meta property="og:site_name" content={siteTitle} />
+        <meta property="og:type" content="website" />
+      </Head>
+      <div className={`fade-animation ${contentVisible ? "hide-content" : ""}`}>
+        {showPreloader && <Preloader />}
+        <div
+          style={{
+            opacity: contentVisible ? 1 : 0,
+            transition: "opacity 0.3s ease-in-out",
+          }}
+        >
+          <Header />
+          <HeroSection />
+          <AboutSection />
+          <PortofolioSection />
+          {/* <TimelineSection /> */}
+          <ContactSection />
+          <Footer />
+        </div>
       </div>
-    </div>
+    </>
   );
 }

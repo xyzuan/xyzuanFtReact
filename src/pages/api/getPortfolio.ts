6e07a44b86f48b2685c0e3a00e2cf1416cb31565
xyzuan <xyzuannihboss@gmail.com>
2023-08-19 14:49:36 +0700

    dev: mongodb init

---
+++ src/pages/api/getPortfolio.ts
@@ -0,0 +1,23 @@
+import { connectDB } from "@/utils/mongo";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const db = await connectDB();
+  const collection = db.collection("portfolio");
+
+  if (req.method === "GET") {
+    try {
+      const portfolioData = await collection.find({}).toArray();
+      res.status(200).json({ portfolioData });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "An error occurred while fetching portfolio data." });
+    }
+  } else {
+    res.status(405).json({ error: "Method not allowed" });
+  }
+}
--- src/pages/sections/Portfolio/PortofolioSection.tsx
+++ src/pages/sections/Portfolio/PortofolioSection.tsx
@@ -1,35 +1,31 @@
 import { Container, Grid, Typography } from "@mui/material";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PortofolioCard from "./components/PortofolioCard";
 
-function PortofolioSection() {
-  const portofolioData = [
-    {
-      title:
-        "2nd Winner of UI/UX Design: Lomba Nasional Kreativitas Mahasiswa LO Kreatif 2022",
-      desc: "foodCare, Inovasi Aplikasi Berbagi Makanan Berlebih pada Masyarakat Sekitar",
-      img: "/assets/lokreatif.png",
-      href: "",
-    },
-    {
-      title: "xdroidOSS Open Source Software",
-      desc: "An Android OS with Minimal Design Oriented which offers a streamlined and simplistic user interface that prioritizes minimalism and ease of use.",
-      img: "/assets/xd.png",
-      href: "https://github.com/xdroid-oss",
-    },
-    {
-      title: "pintarin.",
-      desc: "An online learning platform specifically designed for beginners in programming. It offers a comprehensive range of courses and resources to help individuals kickstart their journey in programming and develop essential coding skills.",
-      img: "/assets/pintarin.png",
-      href: "https://github.com/xyz-prjkt/pintarin",
-    },
-    {
-      title: "jacketLab",
-      desc: "Jacket Lab is a comprehensive and user-friendly application specifically designed to streamline the tasks of assistants when it comes to managing jacket-related information. With Jacket Lab, assistants can effortlessly access a wealth of information regarding jackets, including details about different styles, sizes, materials, and colors.",
-      img: "/assets/jaketlab.png",
-      href: "",
-    },
-  ];
+interface PortfolioItem {
+  title: string;
+  desc: string;
+  img: string;
+  href: string;
+}
+
+const PortofolioSection: React.FC = () => {
+  const [portfolio, setPortfolio] = useState<PortfolioItem[] | undefined>(
+    undefined
+  );
+
+  useEffect(() => {
+    async function fetchPortfolio() {
+      try {
+        const response = await fetch("/api/getPortfolio");
+        const data = await response.json();
+        setPortfolio(data.portfolioData);
+      } catch (error) {
+        console.error("Error fetching portfolio:", error);
+      }
+    }
+    fetchPortfolio();
+  }, []);
 
   return (
     <Container
@@ -60,7 +56,7 @@ function PortofolioSection() {
         grasp of my style
       </Typography>
       <Grid container spacing="24px">
-        {portofolioData.map((item, index) => (
+        {portfolio?.map((item, index) => (
           <Grid item xs={12} sm={6} md={4} key={index}>
             <PortofolioCard
               title={item.title}
@@ -73,6 +69,6 @@ function PortofolioSection() {
       </Grid>
     </Container>
   );
-}
+};
 
 export default PortofolioSection;

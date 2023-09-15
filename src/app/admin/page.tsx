ee302dae862be0b4c888ab087cae362f30eae0ed
xyzuan <xyzuannihboss@gmail.com>
2023-09-15 11:06:57 +0700

    main: Suppress some eslint

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -5,6 +5,7 @@ import { useSession } from "next-auth/react";
 import { signIn } from "next-auth/react";
 
 function page() {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const { status } = useSession({
     required: true,
     onUnauthenticated() {
